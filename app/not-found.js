21809f6023df4c41fb4d2a14d37643af17eb9426
Arsan G <gzf.dev@gmail.com>
2026-06-21 08:49:51 +0330

    Create not-found.js

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center justify-center p-6 bg-white dark:bg-gray-900">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-100">404</h1>
+        <p className="mt-2 text-gray-600 dark:text-gray-300">The page you're looking for doesn't exist.</p>
+      </div>
+    </div>
+  );
+}
